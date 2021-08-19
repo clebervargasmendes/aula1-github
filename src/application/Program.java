9071e5ed02c1190e528b79cdee2d936cbadc9338
Cleber Vargas Mendes <cleber.vargas.mendes@gmail.com>
2021-08-19 15:57:10 -0300

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Aula inicial de Git Github com estrutura do While
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		double C, F;
+		char resp;
+
+		do {
+			System.out.print("Digite a temperatura Celsius:");
+			C = sc.nextDouble();
+			F = C * 9.0 / 5.0 + 32.0;
+			System.out.printf("O equivalente em Fahrenheit é:%.1f%n", F);
+			System.out.print("Deseja repetir: (s/n)?");
+			resp = sc.next().charAt(0);
+		} while (resp != 'n');
+
+		sc.close();
+
+	}
+
+}
